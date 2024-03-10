8e8749c3a59d7aa9faa6e09c21e41cb988593300
Max Saparov <max.saparov@gmail.com>
2024-03-10 15:04:26 -0400

    adding vimrc

--- home.nix
+++ home.nix
@@ -34,11 +34,11 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".vimrc".source = ./dotconf/.vimrc;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-    ".config/nvim".source = dotconf/nvim;
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
